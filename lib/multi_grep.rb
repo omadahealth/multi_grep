69043f433f04a0629ec4eaeef018a30aa6796489
Chris Constantine <chris@omadahealth.com>
2014-02-06 17:13:40 -0800

    Initial version

---
+++ lib/multi_grep.rb
@@ -0,0 +1,48 @@
+require "multi_grep/version"
+
+module MultiGrep
+  class Grep
+    attr_accessor :regex, :matches, :silent
+
+    def initialize
+      self.regex = Regexp.new ''
+      self.matches = Hash.new { Set.new }
+      self.silent = false
+    end
+
+    def silence
+      self.silent = !silent
+    end
+
+    def regex=(r)
+      @regex = Regexp.new r
+    end
+
+    def match_file(filename)
+      open(filename).each_line do |line|
+        if match_data = regex.match(line.strip)
+          all_match = true
+          match_data.names.each do |name|
+            if name[0] == "_"
+              all_match = all_match && matches[name[1..-1]].include?(match_data[name])
+            else
+              matches[name] = matches[name] << match_data[name]
+            end
+          end
+          output "#{filename}: #{line}" if all_match && !silent
+        end
+      end
+      output ""
+      self.silent = false
+    end
+
+    def debug
+      puts "Named captures:"
+      ap matches
+    end
+
+    def output(*arg)
+      puts *arg unless self.silent
+    end
+  end
+end
---
+++ lib/multi_grep/version.rb
@@ -0,0 +1,3 @@
+module MultiGrep
+  VERSION = "0.0.1"
+end
